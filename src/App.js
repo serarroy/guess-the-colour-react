fd6bfa9dae928a8502f1c159aead6d7808026692
Sergio <serarroy@ucm.es>
2022-11-08 18:02:11 +0100

    last commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState} from 'react';
+
 
 function App() {
+
+  const [colour, setColour] = useState('');
+  const [answers, setAnswers] = useState([]);
+  const [isCorrectAnswer, setIsCorrectAnswer] = useState(false);
+
+  const getRandomColour = () => {
+    let characters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
+
+    const colour = new Array(6).fill('').map(() => characters[Math.floor(Math.random() * characters.length)]).join('')
+    return `#${colour}`
+  }
+
+  const generateColours = () => {
+    const actualColour = getRandomColour()
+    setColour(actualColour)
+    setAnswers([actualColour, getRandomColour(), getRandomColour()].sort(() => 0.5 - Math.random())) // Colocar el array de respuestas de forma aleatoria
+  }
+
+  useEffect(() => {
+    generateColours()
+  }, [])
+
+  const handleClick = (answer) => {
+    if(answer === colour){
+      setIsCorrectAnswer(true)
+      generateColours()
+    }
+    else{
+      setIsCorrectAnswer(false)
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Guess the colour</h1>
+      <div className='colour-container' style={{background: colour}}></div>
+        { answers.map((answer) => (
+          <button key={answer} onClick={() => handleClick(answer)}>{answer}</button>
+        ))}
+      
+        { isCorrectAnswer? <div className='correct'>Correct Answer</div>:
+        <div className='wrong'>Wrong Answer</div>
+        }
     </div>
   );
 }
